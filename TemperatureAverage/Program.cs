3036f6f025dd47932efa3ff88b8a48c192ff3a5f
aditya raj <adityar610@gmail.com>
2023-01-19 23:44:04 +0530

    properly working

--- TemperatureAverage/Program.cs
+++ TemperatureAverage/Program.cs
@@ -16,10 +16,26 @@ namespace exercise_state_wise_temperature_average
              *second state provides temperature of 7 cities
              *third state provides temperature of 4 cities
             */
+            int[][] statesTemprature = new int[3][];
+            statesTemprature[0] = new int[3];
+            statesTemprature[1] = new int[7];
+            statesTemprature[2] = new int[4];
 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < statesTemprature[i].Length; j++)
+                {
+                    Console.WriteLine($"Give tempreture for city {j + 1} of state {i + 1} :");
+                    statesTemprature[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
             /*
              * Call GetStateWiseAverage() with state position and temperature data
              */
+            Console.WriteLine("Which state's average do you want to calculate?  1, 2, 3 ");
+            int stateNum = int.Parse(Console.ReadLine());
+            int average = GetStateWiseAverage(stateNum, statesTemprature);
+            Console.WriteLine($"the tempreture average for state {stateNum} is  {average}");
 
             /*
              * Display Average
@@ -30,10 +46,16 @@ namespace exercise_state_wise_temperature_average
          * Method calculates average of temperature for the state whose position is provided
          */
 
-        public static int GetStateWiseAverage()
+        public static int GetStateWiseAverage(int ststePosition, int[][] stateTemp)
         {
             // put here logic to calculate average
-            return 0;
+            int totalCity = stateTemp[ststePosition - 1].Length;
+            int sum = 0;
+            for (int i = 0; i < totalCity; i++)
+            {
+                sum += stateTemp[ststePosition - 1][i];
+            }
+            return sum / totalCity;
         }
     }
 }
